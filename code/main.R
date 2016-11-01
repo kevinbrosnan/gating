475a72f750dac503222dd4aa461fcd987286312b
Kevin Brosnan <kevin.c.brosnan@ul.ie>
2016-11-01 17:06:18 +0000

    Tidy up of code

--- code/main.R
+++ code/main.R
@@ -1,6 +1,6 @@
 #------------------------------------------------------------------------------#
 #                  Automated Gating of Flow Cytometry Data via                 #
-#                   Adaptive Markov Random Fields Clustering                   #
+#                        Markov Random Fields Clustering                       #
 #                                                                              #
 # Authors: Mr. Kevin Brosnan, Maths & Stats, University of Limerick            #
 #          Dr. Norma Bargary, Maths & Stats, University of Limerick            #
@@ -15,64 +15,108 @@
 
 ####--- Set Up -------------------------------------------------------------####
 
-  # Required Functions
-  functions.path <- "functions"
-  sub.dir <- dir(path = functions.path, recursive = TRUE, full.names = TRUE)
-  invisible(sapply(sub.dir, source))
-  rm(functions.path, sub.dir)
-  
   # Required Libraries
-  #install.packages(c('tools', 'stargazer', 'SDMTools'))
-  library(tools)
+  # install.packages(c('stargazer', 'SDMTools'))
   library(SDMTools)
   library(stargazer)
+  library(flowClust)
 
-####--- Rituximab Data Example ---------------------------------------------####
-
-  # Read in Rituximab Data - online resource
-  github_account <- "https://raw.githubusercontent.com/significantstats/"
-  file_location <- "automatedgating/master/rituximab.csv"
-  rituximab <- read.csv(file = paste0(github_account, file_location))
+  # GitHub Access
+  gh.proj <- "https://raw.githubusercontent.com/significantstats/gating/master/"
+  gh.code <- paste0(gh.proj, "code/functions/")
+  gh.data <- paste0(gh.proj, "data/")
   
-  # Exploratory Analysis
+  # Required Data
+  GvHD.pos <- read.csv(paste0(gh.data, "GvHD/GvHD_positive.csv"))
+  GvHD.con <- read.csv(paste0(gh.data, "GvHD/GvHD_control.csv"))
+  rituximab <- read.csv(paste0(gh.data, "rituximab/rituximab.csv"))
+  
+  # Required Functions
+  source(paste0(gh.code, "explore_flow.R"))
+  source(paste0(gh.code, "make_grid.R"))
+  source(paste0(gh.code, "unmake_grid.R"))
+  source(paste0(gh.code, "mrf_gating.R"))
+  
+####--- Exploratory Analysis (Rituximab Data) ------------------------------####
+  
+  # Manual Gating Example
+  par(pty = "s")
+  plot(rituximab[ , c("FSC.H", "SSC.H")], xlab = "FSC-Height", 
+       ylab = "SSC-Height", las = 1, col = "gray")
+  polygon(x = c(50,50,440,380), y = c(0,200,580,80), 
+          border = "red", lwd = 2, lty = 1)
+  polygon(x = c(50,50,420,600), y = c(0,200,610,110), 
+          border = "blue", lwd = 2, lty = 2)
+  legend("topright", legend = c("Expert 1", "Expert 2"), 
+         col = c("red", "blue"), lty = c(1,2), lwd = c(2,2))
+  par(pty = "m")
+  
+  # t-mixtures approach
+  model <- flowClust(rituximab, varNames = c("FSC.H", "SSC.H"), K = 1:6)
+  best <- which.max(criterion(object = model, "BIC"))
+  plot(model[[best]], data = rituximab, las = 1,
+       xlab = "FSC-Height", ylab = "SSC-Height")
+  
+  # Descriptive Table & Plots
   explore_flow(rituximab)
   stargazer(rituximab, summary = TRUE, digits = 2, label = "tab:ritdatasum", 
             median = TRUE, iqr = TRUE, mean.sd = FALSE,
             title = "Summary Statistics for Rituximab Data", align = TRUE)
   
-  # Gating FSC v SSC
-  gate.initial <- mrf_gating(rituximab[,c('FSC.H', 'SSC.H')])
-  plot(gate.initial)
+####--- Figures & Tables from Paper ----------------------------------------####
   
-  # Gating 7 AAD v Anti-BrdU FITC
-  gate.analysis <- mrf_gating(rituximab[which(gate.initial$groups == 1),c('FL1.H', 'FL3.H')])
-  plot(gate.analysis)
-
-####--- GvHD Data Example --------------------------------------------------####
-
-  # Read in Rituximab Data - online resource
-  github_account <- "https://raw.githubusercontent.com/significantstats/"
-  file_location <- "automatedgating/master/"
-  gvhd.con <- read.csv(file = paste0(github_account, file_location, 'GvHD_control.csv'))
-  gvhd.pos <- read.csv(file = paste0(github_account, file_location, 'GvHD_positive.csv'))
+  # Figure 1 - Rituximab Data FSC-Height v SSC-Height
+  Lo.initial <- flowClust(rituximab, varNames = c("FSC.H", "SSC.H"), K = 1, 
+                          B = 100, z.cutoff = 0.5)
   
-  # Exploratory Analysis
-  explore_flow(gvhd.con)
-  stargazer(gvhd.con, summary = TRUE, digits = 2, label = "tab:gvhd.con", 
-            median = TRUE, iqr = TRUE, mean.sd = FALSE,
-            title = "Summary Statistics for GvHD Control Data", align = TRUE)
+  MRF.initial <- mrf_gating(rituximab[, c("FSC.H", "SSC.H")])
   
-  explore_flow(gvhd.pos)
-  stargazer(gvhd.pos, summary = TRUE, digits = 2, label = "tab:gvhd.pos", 
-            median = TRUE, iqr = TRUE, mean.sd = FALSE,
-            title = "Summary Statistics for GvHD Positive Data", align = TRUE)
+  par(mfrow = c(1,2))
+  par(pty = "s")
+  plot(Lo.initial, data = rituximab, xlab = "FSC-Height", 
+       ylab = "SSC-Height", las = 1, col = 'white', 
+       main = "(a) t mixture with Box-Cox", show.outliers = FALSE)
+  points(rituximab[which(Lo.initial@flagOutliers), c(1,2)], 
+         col = gray(3/4), pch = 20)
   
-  # Gating FSC v SSC
-  gate.initial <- mrf_gating(rituximab[,c('FSC.H', 'SSC.H')])
-  plot(gate.initial)
+  par(pty = "s")
+  plot(MRF.initial$x, type = "n", las = 1)
+  points(MRF.initial$x[which(MRF.initial$removals == 1),], pch = "*")
   
-  # Gating 7 AAD v Anti-BrdU FITC
-  gate.analysis <- mrf_gating(gate.initial$population[,c('FL1.H', 'FL3.H')])
-  plot(gate.analysis)
-
-
+  
+  par(mfrow = c(1, 1))
+  
+  # Figure 2 - Rituximab Data 7-AAD v Anti-BrdU
+  Lo.gate <- rituximab[rituximab %in% Lo.initial, c("FL3.H", "FL1.H")]
+  Lo.7AAD_antiBrdU <- flowClust(Lo.gate, K = 3, B = 100)
+  
+  MRF.gate <- rituximab[which(MRF.initial$groups == 1), c("FL3.H", "FL1.H")]
+  MRF.7AAD_antiBrdU <- mrf_gating(MRF.gate)
+  
+  par(mfrow = c(1,2))
+  par(pty = "s")
+  plot(Lo.7AAD_antiBrdU, data = Lo.gate, ylab = 'Anti-BrdU FITC', 
+       xlab = '7 AAD', main = "(a) t mixture with Box-Cox", col = 'white', 
+       las = 1, show.outliers = FALSE)
+  points(Lo.gate[which(Lo.7AAD_antiBrdU@flagOutliers),], 
+         col = gray(3/4), pch = 20)
+  
+  par(pty = 's')
+  
+  par(mfrow = c(1, 1))
+  
+  
+  # Table 1 - Rituximab Data
+  
+  
+  
+  
+  # Figure 3 - GvHD Data Control Group CD4 v CD8B, CD4 v CD3
+  
+  
+  # Figure 4 - GvHD Data Positive Group CD4 v CD8B, CD4 v CD3
+  
+  
+  # Table 2 - GvHD Data
+  
+####--- End of Script ------------------------------------------------------####  
