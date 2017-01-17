36371845b92f706665aa6cbf61dffd008ac2265a
Kevin-Brosnan <kevin.c.brosnan@gmail.com>
2017-01-17 23:29:51 +0000

    Update for %% and which commands

--- code/functions/neighbours.R
+++ code/functions/neighbours.R
@@ -1,15 +1,16 @@
 neighbours <- function(x, position, value, dimension) {
   
-  if (position %% dimension == 0) {
+  top.bottom.cases <- position %% dimension
+  if (top.bottom.cases == 0) {
     neighbourhood <- position + c(-1, -dimension, dimension)
-  } else if ((position - 1) %% dimension == 0) {
+  } else if (top.bottom.cases == 1) {
     neighbourhood <- position + c(1, -dimension, dimension)
   } else {
     neighbourhood <- position + c(1, -1, dimension, -dimension)
   }
   
-  neighbourhood <- neighbourhood[which(neighbourhood > 0)]
-  neighbourhood <- neighbourhood[which(neighbourhood <= dimension ^ 2)]
+  neighbourhood <- neighbourhood[neighbourhood > 0]
+  neighbourhood <- neighbourhood[neighbourhood <= dimension ^ 2]
   
   neighbours <- sum(x[neighbourhood] == value)
   
