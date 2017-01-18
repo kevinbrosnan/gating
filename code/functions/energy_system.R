6c11b2ae20ef1c8f6a03a48d4765c3adfd83ec43
Kevin-Brosnan <kevin.c.brosnan@gmail.com>
2017-01-18 11:14:36 +0000

    Speed updates

--- code/functions/energy_system.R
+++ code/functions/energy_system.R
@@ -3,21 +3,15 @@ energy_system <- function(x, value = NULL, dimension) {
   if (is.null(value)) {
   
     energy <- matrix(0, nrow = dimension, ncol = dimension)
-    for (i in 1:dimension) {
-      for (j in 1:dimension) {
-        pos <- j + (i - 1) * dimension
-        energy[pos] <- energy(x, position = pos, value = x[j,i], dimension)
-      }
+    for (i in 1:(dimension^2)) {
+      energy[i] <- energy(x, position = i, value = x[i], dimension)
     }
 
   } else {
     
     energy <- matrix(0, nrow = dimension, ncol = dimension)
-    for (i in 1:dimension) {
-      for (j in 1:dimension) {
-        pos <- j + (i - 1) * dimension
-        energy[pos] <- energy(x, position = pos, value = value)
-      }
+    for (i in 1:(dimension^2)) {
+      energy[i] <- energy(x, position = i, value = value)
     }
     
   }
