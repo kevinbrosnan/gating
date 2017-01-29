dbbdeb88824260d15a4ffffa89dab9c74094aa7d
Kevin-Brosnan <kevin.c.brosnan@gmail.com>
2017-01-29 16:37:29 +0000

    Update to spatial smooth

--- code/functions/spatial_smooth.R
+++ code/functions/spatial_smooth.R
@@ -3,10 +3,12 @@ spatial_smooth <- function(x) {
   no.row <- nrow(x)
   no.col <- ncol(x)
 
+  x <- x
   
   for (i in 2:(no.row - 1)) {
     for (j in 2:(no.col - 1)) {
-      x[i,j] <- (x[i,j] + 0.5 * (x[(i - i), j] + x[i, (j + 1)] + x[(i + 1), j] + x[i, (j - 1)]) + (1/sqrt(2)) * (x[(i - 1), (j - 1)] + x[(i - 1), (j + 1)] + x[(i + 1), (j + 1)] + x[(i + 1), (j - 1)]))/(3 + sqrt(2))
+      x[i,j] <- x[i,j] + 0.5 * (x[(i - 1), j] + x[(i + 1), j] + x[i, (j - 1)] + x[i, (j + 1)]) + (1/(2*sqrt(2))) * (x[(i - 1), (j - 1)] + x[(i - 1), (j + 1)] + x[(i + 1), (j + 1)] + x[(i + 1), (j - 1)])
+      x[i,j] <- x[i,j]/(3 + sqrt(2))
     }
   }  
   
