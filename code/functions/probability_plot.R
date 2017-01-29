49a30816d60fb618e48cdb92ec6132792bfa58c3
Kevin-Brosnan <kevin.c.brosnan@gmail.com>
2017-01-29 13:11:32 +0000

    Add points argument and change colouring

--- code/functions/probability_plot.R
+++ code/functions/probability_plot.R
@@ -1,4 +1,4 @@
-probability_map <- function(x) {
+probability_map <- function(x, data = NULL) {
   
   no.row <- nrow(x)
   no.col <- ncol(x)
@@ -8,18 +8,20 @@ probability_map <- function(x) {
   x.pos <- rep(1:no.row, times = no.col)
   y.pos <- sort(rep(1:no.col, times = no.row))
   val <- as.vector(x)
-  val.col <- ifelse(val < 0.25, colours.scale[1], 
-                    ifelse(val < 0.5, colours.scale[2], 
-                           ifelse(val == 0.5, colours.scale[3],
-                                  ifelse(val < 0.75, colours.scale[4],
-                                         colours.scale[5]))))
+  val.col <- as.factor(val)
+  
   par(pty = "s")
   plot(x.pos, y.pos, type = "n", ylim = c(-0.5, no.col), xlim = c(-0.5, no.col), las = 1)
   
   for (i in 1:length(x.pos)) {
     polygon(x = c((x.pos[i] - 1.5), (x.pos[i] - 1.5), (x.pos[i] - 0.5), (x.pos[i] - 0.5)), 
             y = c((y.pos[i] - 1.5), (y.pos[i] - 0.5), (y.pos[i] - 0.5), (y.pos[i] - 1.5)),
-            col = val.col[i], border = NA)
+            col = colours.scale[val.col[i]], border = NA)
   }
+  
+  if (!is.null(data)) {
+    points(data)
+  }
+  
   return(invisible(NULL))
 }
