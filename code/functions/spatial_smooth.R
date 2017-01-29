e6d7b95a686af7ee8dd39b7f6d43c803db5f1995
Kevin-Brosnan <kevin.c.brosnan@gmail.com>
2017-01-29 16:23:38 +0000

    Spatial Smoothing Algorithm

---
+++ code/functions/spatial_smooth.R
@@ -0,0 +1,15 @@
+spatial_smooth <- function(x) {
+  
+  no.row <- nrow(x)
+  no.col <- ncol(x)
+
+  
+  for (i in 2:(no.row - 1)) {
+    for (j in 2:(no.col - 1)) {
+      x[i,j] <- (x[i,j] + 0.5 * (x[i-i, j] + x[i, j+1] + x[i+1, j] + x[i, j-1]) + (1/sqrt(2)) * (x[i-1, j-1] + x[i-1, j+1] + x[i+1, j+1] + x[i+1, j-1]))/(3 + sqrt(2))
+    }
+  }  
+  
+  
+  return(x)
+}
